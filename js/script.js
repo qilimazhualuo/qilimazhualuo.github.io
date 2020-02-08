f4624f573c6945376896d2e7cd8ea973e585d3e2
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2020-02-08 17:12:54 +0800

    html

---
+++ js/script.js
@@ -0,0 +1,58 @@
+function bf(){
+	var audio = document.getElementById("music1");
+	audio.play();
+}
+function pause(){
+	var audio = document.getElementById("music1");
+	audio.pause();
+}
+function prev(){
+	var audio = document.getElementById("music1");
+	
+}
+function next(){
+	var audio = document.getElementById("music1");
+	
+}
+function one(){
+	var audio = document.getElementById("music1");
+	var showImage = document.getElementById("show");
+	showImage.src = "img/1.jpg";
+	audio.src="audio/1.mp3";
+	audio.play();
+}
+function two(){
+	var audio = document.getElementById("music1");
+	var showImage = document.getElementById("show");
+	showImage.src = "img/2.jpg";
+	audio.src="audio/2.mp3";
+	audio.play();
+}
+function three(){
+	var audio = document.getElementById("music1");
+	var showImage = document.getElementById("show");
+	showImage.src = "img/3.jpg";
+	audio.src="audio/3.mp3";
+	audio.play();
+}
+function four(){
+	var audio = document.getElementById("music1");
+	var showImage = document.getElementById("show");
+	showImage.src = "img/4.jpg";
+	audio.src="audio/4.mp3";
+	audio.play();
+}
+function five(){
+	var audio = document.getElementById("music1");
+	var showImage = document.getElementById("show");
+	showImage.src = "img/5.jpg";
+	audio.src="audio/5.mp3";
+	audio.play();
+}
+function six(){
+	var audio = document.getElementById("music1");
+	var showImage = document.getElementById("show");
+	showImage.src = "img/6.jpg";
+	audio.src="audio/6.mp3";
+	audio.play();
+}

5d6e8f1417dfb2a7cb17ef687a013573e082d45d
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2020-02-09 16:15:06 +0800

    更新

--- html02/js/script.js
+++ html02/js/script.js
@@ -2,3 +2,4 @@ function load(){
 	var plate = document.getElementById("plate")
 	plate.width = "80%"
 }
+
---
+++ html03/js/script.js
@@ -0,0 +1,5 @@
+function load(){
+	var plate = document.getElementById("plate")
+	plate.width = "80%"
+}
+
